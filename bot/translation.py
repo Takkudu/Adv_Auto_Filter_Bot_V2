31426ba2b5e8b61d8d7c020ea2d8fad32f8654db
Takkudu <88140618+Takkudu@users.noreply.github.com>
2021-09-23 21:38:59 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -47,11 +47,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/anilsebastian">AnilSebastian</a></i></b>
+<b>➥ 💻Creator</b> : <b><i><a href="https://t.me/anilsebastian">AnilSebastian</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ 🌏Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ 📚Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ 📋Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">🔘Click Me</a></i>
 """
