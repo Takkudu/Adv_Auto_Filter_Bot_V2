f85175444892b660044e9a36d5f4fddee54b92df
Takkudu <88140618+Takkudu@users.noreply.github.com>
2021-10-03 15:32:54 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG
 
-class Translation(object):
-   
-  await cmd.reply_photo(
+class Translation(object):  
+  reply_photo(
     photo="https://telegra.ph/file/bf038c38666d51adeada9.jpg",
     START_TEXT = """<b>Hey {} How Are You!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
