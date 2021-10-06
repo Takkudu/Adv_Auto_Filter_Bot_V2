16985b309b6d6951f6d98e66e01002b5bb22d686
Takkudu <88140618+Takkudu@users.noreply.github.com>
2021-10-06 17:06:02 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -209,7 +209,8 @@ async def auto_filter(bot: Client, update: Message):
         reply_markup = InlineKeyboardMarkup(result[0])
 
         try:
-            await bot.send_message(
+            await bot.send_photo(
+                photo= MASSAGE_PHOTO,
                 chat_id = update.chat.id,
                 text=f"<b>🌩 𝐓𝐨𝐭𝐚𝐥 𝐑𝐞𝐬𝐮𝐥𝐭𝐬: {(len_results)} </b>\n<b>🎬 𝐌𝐨𝐯𝐢𝐞/𝐒𝐞𝐫𝐢𝐞𝐬:</b> <code>{query}</code>",
                 reply_markup=reply_markup,
