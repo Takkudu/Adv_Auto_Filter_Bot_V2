4fb50b7ffba692f6008b21bfbeb2b9f68918b1f9
Takkudu <88140618+Takkudu@users.noreply.github.com>
2021-10-19 09:57:14 +0530

    Create Config.py

---
+++ bot/plugins/Config.py
@@ -0,0 +1,12 @@
+START_IMG = environ.get("START_IMG", "") 
+
+FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+if FILE_CAPTION.strip() == "":
+    CUSTOM_FILE_CAPTION=None
+else:
+    CUSTOM_FILE_CAPTION=FILE_CAPTION
+if OMDB_API_KEY.strip() == "":
+    API_KEY=None
+else:
+    API_KEY=OMDB_API_KEY
