3c7a287c373126fbd5c7f8fa8e15b17799b4d174
Takkudu <88140618+Takkudu@users.noreply.github.com>
2021-10-19 10:01:17 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -57,13 +57,8 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
-                update.from_user.first_name),
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
+    await message.reply_photo(photo=START_IMG, caption=START_MSG.format(message.from_user.mention),
+            reply_markup=InlineKeyboardMarkup(
     )
 
 
