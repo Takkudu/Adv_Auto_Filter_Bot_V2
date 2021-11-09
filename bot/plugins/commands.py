9688d41dd1c87ef7ee273242319a795f2c39eb2e
Takkudu <88140618+Takkudu@users.noreply.github.com>
2021-11-09 23:35:53 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption ="<b>caption\n\n𝙅𝙊𝙄𝙉 ❤️@CinemaFactory_All😉</b>",
+                caption =caption,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
